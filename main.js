ea81afc247121cbfeb041f56b69adccd5a1d3819
Mikaela Matthews <mikaelamatthews@Mikaelas-MacBook-Air.local>
2025-12-24 02:37:39 -0600

    added current date and current time within create div container

--- main.js
+++ main.js
@@ -1,21 +1,40 @@
 const timeContainer = document.getElementById('timeContainer')
 const createDiv = document.createElement('div')
 
-function createElements() {
+function createTimeElements() {
     createDiv.style.width = "500px"
     createDiv.style.height = "150px"
-    createDiv.style.padding = "15px"
+    createDiv.style.padding = "5px"
     createDiv.style.border = "solid 5px #FFFF"
     createDiv.style.borderRadius = "10px"
     createDiv.style.fontFamily = "'Berkshire Swash', Arial, Helvetica, sans-serif"
-    createDiv.style.color = "#FFF"
-    createDiv.style.fontSize = "70px"
+    createDiv.style.color = "#fff"
+    createDiv.style.fontSize = "50px"
     createDiv.style.textAlign = "center"
     timeContainer.appendChild(createDiv)
 }
 
+function currentTime() {
+    const currDay = new Date()
+    const currTime = new Date()
 
-function clockTracker() {
+    const year = currDay.getFullYear()
+    const month = currDay.getMonth()
+    const day = currDay.getDate()
+
+    const hours = currTime.getHours()
+    const mins = currTime.getMinutes()
+    const secs = currTime.getSeconds()
+
+    const timeFormat = `${String(hours).padStart(2, "0")}:${(String(mins).padStart(2, "0"))}:${(String(secs).padStart(2, "0"))}`
+
+    const dayFormat = `${String(year)}-${(String(month + 1).padStart(2, "0"))}-${(String(day).padStart(2, "0"))}`
+    
+    createDiv.innerHTML = `${dayFormat}<br>${timeFormat}`
+}
+
+/*
+function currentTime() {
     const currTime = new Date()
     
     const hours = currTime.getHours()
@@ -26,8 +45,8 @@ function clockTracker() {
 
     createDiv.textContent = timeFormat
 }
-
-createElements()
+*/
+createTimeElements()
 
 // Live count
-setInterval(clockTracker, 1000);
+setInterval(currentTime, 1000);
