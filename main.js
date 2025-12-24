c05f29484d1b71736a9bf8a1bfe12be2e8979f35
Mikaela Matthews <mikaelamatthews@Mikaelas-MacBook-Air.local>
2025-12-24 02:17:45 -0600

    clock is set to current time

--- main.js
+++ main.js
@@ -4,13 +4,18 @@ const createDiv = document.createElement('div')
 function createElements() {
     createDiv.style.width = "500px"
     createDiv.style.height = "150px"
+    createDiv.style.padding = "15px"
     createDiv.style.border = "solid 5px #FFFF"
     createDiv.style.borderRadius = "10px"
+    createDiv.style.fontFamily = "'Berkshire Swash', Arial, Helvetica, sans-serif"
+    createDiv.style.color = "#FFF"
+    createDiv.style.fontSize = "70px"
+    createDiv.style.textAlign = "center"
     timeContainer.appendChild(createDiv)
 }
 
 
-function countdownTimer() {
+function clockTracker() {
     const currTime = new Date()
     
     const hours = currTime.getHours()
@@ -19,10 +24,10 @@ function countdownTimer() {
 
     const timeFormat = `${String(hours).padStart(2, "0")}:${(String(mins).padStart(2, "0"))}:${(String(secs).padStart(2, "0"))}`
 
-    createDiv.textContent = timeFormat;
+    createDiv.textContent = timeFormat
 }
 
 createElements()
 
 // Live count
-//setInterval(countdownTimer, 1000);
+setInterval(clockTracker, 1000);
